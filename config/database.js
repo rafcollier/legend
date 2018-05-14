e6d5c1181b8d255448205e847d6ddde56975a05e
rafcollier <rogercollier@hotmail.com>
2018-05-14 12:50:01 -0400

    added mongodb heroku URI

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
  //database: 'mongodb://localhost:27017/legend',
-  database: 'mongodb://heroku_n29prwpc:ul2kl1qsprbtn39bsf28d4qfhb@ds123490.mlab.com:23490/heroku_k52f5vsp',
+  database: 'mongodb://heroku_k52f5vsp:efhqgaaq5augddhfj8g61hb7l4@ds123490.mlab.com:23490/heroku_k52f5vsp',
   secret: 'legendSecret'
 }
