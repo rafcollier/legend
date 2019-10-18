86839071ede8b96bd3d0331a9f8ee66af7abfa62
Roger Collier <rogercollier@hotmail.com>
2019-10-18 15:26:00 -0400

    Delete database.js

--- config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  //database: 'mongodb://localhost:27017/legend',
-  database: 'mongodb://svc-joule-legend:TUxIakNOT0RwbncqZHprZTA5bVhNSnJ4cmhxcCNfZSpMbCpoS2dANQo@localhost:27020/legend',
-  //database: 'mongodb://heroku_k52f5vsp:efhqgaaq5augddhfj8g61hb7l4@ds123490.mlab.com:23490/heroku_k52f5vsp',
-  secret: 'legendSecret'
-}
